c44a489a022148479ebea155d79761e9f8fb36c8
DESKTOP-90IPBBT\Trinh The Hung <trinhthehungit@gmail.com>
2022-04-12 23:11:36 +0700

    commit

--- src/Services/Catalog/Catalog.API/Controllers/WeatherForecastController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/WeatherForecastController.cs
@@ -19,13 +19,14 @@ namespace Catalog.API.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Enumerable.Range(1, 5)
+                            .Select(index => new WeatherForecast
+                                                {
+                                                    Date = DateTime.Now.AddDays(index),
+                                                    TemperatureC = Random.Shared.Next(-20, 55),
+                                                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                                                })
+                            .ToArray();
         }
     }
 }
